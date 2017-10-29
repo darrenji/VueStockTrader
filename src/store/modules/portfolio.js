e6b5cb0b10e14cb2ef1473036a724acac30437a9
darren <764190362@qq.com>
2017-10-29 09:06:41 +0800

    有关portfolio的module完成

---
+++ src/store/modules/portfolio.js
@@ -0,0 +1,61 @@
+const state = {
+    funds: 10000,
+    stocks:[]
+};
+
+const mutations = {
+    'BUY_STOCK'(state, {stockId, quantity, stockPrice}){
+        const record = state.stocks.find(e => e.id == stockId);
+        if(record){
+            record.quantity += quantity;
+        } else {
+            state.stocks.push({
+                id: stockId,
+                quantity: quantity
+            });
+        }
+        state.funds -= stockPrice * quantity;
+    },
+    'SELL_STOCK'(state, {stockId, quantity, stockPrice}){
+        const record = state.stocks.find(e => e.id == stockId);
+        if(record.quantity > quantity){
+            record.quantity -= quantity;
+        } else {
+            //如果购买的数量大于现有的数量，就把当前数组中的这个对象删除
+            state.stocks.splice(state.stocks.indexOf(record), 1);
+        }
+        state.funds += stockPrice * quantity;
+    }
+};
+
+const actions = {
+    sellStock({commit}, order){
+        commit('SELL_STOCK', order);
+    }
+};
+
+const getters = {
+  stockPortfolio(state, getters){
+      return state.stocks.map(stock => {
+          const record = getters.stocks.find(e => e.id == stock.id);
+          return {
+              id: stock.id,
+              quantity: stock.quantity,
+              name: record.name,
+              price: record.price
+          }
+      });
+  },
+  funds(state){
+      return state.funds;
+  }
+};
+
+
+export default {
+    state,
+    mutations,
+    actions,
+    getters
+}
+
